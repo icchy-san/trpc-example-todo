d513fe949523ccbc3b35220b3ac525d26d057517
icchy-san <icchy.tech@gmail.com>
2024-03-24 08:53:49 +0900

    modified rpc name

--- server/router.ts
+++ server/router.ts
@@ -7,7 +7,7 @@ import  { type Todo, todoService } from './service'
 export const t = initTRPC.create()
 
 export const appRouter = t.router({
-    getTodoList: t.procedure.query((opts) => {
+    todoList: t.procedure.query((opts) => {
         return todoService.getAll()
     }),
     addTodo: t.procedure
